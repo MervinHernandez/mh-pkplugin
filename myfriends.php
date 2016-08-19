c04b48df8a84d8091de165d7c3711f81a4fa344d
Mervin Hernandez <me@mervinhernandez.com>
2016-08-19 00:51:51 -0400

    Added definition and field

--- myfriends.php
+++ myfriends.php
@@ -1,33 +1,32 @@
 <?php
 /***
-My Books POST TYPE
-+ Meta-box: mybook-details
+My Friends POST TYPE
 ***/
-  add_filter('piklist_post_types', 'mybooks_post_type');
-  function mybooks_post_type($post_types)
+  add_filter('piklist_post_types', 'myfriends_post_type');
+  function myfriends_post_type($post_types)
   {
-    $post_types['mybook'] = array(
-      'labels' => piklist('post_type_labels', 'My Books')
-      ,'title' => __('My Library of books')
+    $post_types['myfriend'] = array(
+      'labels' => piklist('post_type_labels', 'My Friends')
+      ,'title' => __('My friends and colleagues')
       // ,'menu_icon' => piklist('url', 'piklist') . '/parts/img/piklist-menu-icon.svg'
       // ,'page_icon' => piklist('url', 'piklist') . '/parts/img/piklist-page-icon-32.png'
       ,'supports' => array(
         'title'
         ,'editor'
-        , 'author'
-        , 'excerpt'
+        ,'author'
+        ,'excerpt'
       )
       ,'public' => true
       ,'admin_body_class' => array(
-        'mybooks'
+        'myfriends'
       )
       ,'has_archive' => true
       ,'rewrite' => array(
-        'slug' => 'mybooks'
+        'slug' => 'myfriends'
       )
       ,'capability_type' => 'post'
       ,'edit_columns' => array(
-        'title' => __('Title')
+        'title' => __('Name')
       )
       ,'hide_meta_box' => array(
         //Metaboxes to hide go here
@@ -35,16 +34,17 @@ My Books POST TYPE
     );
     return $post_types;
   }
-/* Book Author Field */
-  function my_add_sub_title() {
+
+/* Friend Subtitle Field */
+  function myfriend_subtitle_add() {
   piklist('field', array(
     'type' => 'text'
-    ,'field' => 'mybook_author'
+    ,'field' => 'myfriend_subtitle'
     ,'template' => 'field' // format the field without a label
     ,'attributes' => array(
       'class' => 'large-text'
-      ,'placeholder' => 'Book Author'
+      ,'placeholder' => 'My Friend subtitle'
     )
   ));
 }
-add_action('edit_form_after_title', 'my_add_sub_title');
+add_action('edit_form_after_title', 'myfriend_subtitle_add');
