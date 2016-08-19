cdc2ae308eee7ebdb4c832991b159190ca782fa6
Mervin Hernandez <me@mervinhernandez.com>
2016-08-18 22:53:34 -0400

    bumped version number

--- mh-pkplugin.php
+++ mh-pkplugin.php
@@ -3,7 +3,7 @@
 Plugin Name: MH.com PIKLIST Plugin
 Plugin URI: http://piklist.com
 Description: Adds the custom PikList-enabled functionality and features to my site
-Version: 0.0.1
+Version: 0.0.2
 Author: Mervin Hernandez
 Author URI: http://mervinhernandez.com
 Plugin Type: Piklist
