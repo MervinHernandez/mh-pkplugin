b3a087a46035bba9848a3906fee00fd7090e8a09
Mervin Hernandez <me@mervinhernandez.com>
2016-08-19 00:42:28 -0400

    Split up the main piklist function file

--- mh-pkplugin.php
+++ mh-pkplugin.php
@@ -10,56 +10,7 @@ Plugin Type: Piklist
 Text Domain: mh-pkplugin
 */
 if (!defined('ABSPATH'))
-  {
-    exit;
-  }
+  {exit;}
 
-/*** My Books POST TYPE
-        + Meta-box: mybook-details
-***/
-  add_filter('piklist_post_types', 'mybooks_post_type');
-  function mybooks_post_type($post_types)
-  {
-    $post_types['mybook'] = array(
-      'labels' => piklist('post_type_labels', 'My Books')
-      ,'title' => __('My Library of books')
-      // ,'menu_icon' => piklist('url', 'piklist') . '/parts/img/piklist-menu-icon.svg'
-      // ,'page_icon' => piklist('url', 'piklist') . '/parts/img/piklist-page-icon-32.png'
-      ,'supports' => array(
-        'title'
-        ,'editor'
-        , 'author'
-        , 'excerpt'
-      )
-      ,'public' => true
-      ,'admin_body_class' => array(
-        'mybooks'
-      )
-      ,'has_archive' => true
-      ,'rewrite' => array(
-        'slug' => 'mybooks'
-      )
-      ,'capability_type' => 'post'
-      ,'edit_columns' => array(
-        'title' => __('Title')
-      )
-      ,'hide_meta_box' => array(
-        //Metaboxes to hide go here
-      )
-    );
-    return $post_types;
-  }
-/* Book Author Field */
-  function my_add_sub_title() {
-  piklist('field', array(
-    'type' => 'text'
-    ,'field' => 'mybook_author'
-    ,'template' => 'field' // format the field without a label
-    ,'attributes' => array(
-      'class' => 'large-text'
-      ,'placeholder' => 'Book Author'
-    )
-  ));
-}
-add_action('edit_form_after_title', 'my_add_sub_title');
-/*** End of MyBooks ***/
+/*** myBooks - Post Type Definition ***/
+include 'mybooks.php';
