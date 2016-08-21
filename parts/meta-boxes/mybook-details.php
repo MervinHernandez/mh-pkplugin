d7ef583e22e2ac780e170371e975a7a0fd2b0147
Mervin Hernandez <me@mervinhernandez.com>
2016-08-21 19:59:59 -0400

    moved the field to the metabox file

--- parts/meta-boxes/mybook-details.php
+++ parts/meta-boxes/mybook-details.php
@@ -5,7 +5,19 @@ Post Type: mybook
 Meta Box: true
 Lock: true
 */
-
+/* Book Author Field */
+  function my_add_sub_title() {
+  piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'mybook_author'
+    ,'template' => 'field' // format the field without a label
+    ,'attributes' => array(
+      'class' => 'large-text'
+      ,'placeholder' => 'Book Author'
+    )
+  ));
+}
+add_action('edit_form_after_title', 'my_add_sub_title');
 // Published Date : full date picker
   piklist('field', array(
      'type' => 'datepicker',
