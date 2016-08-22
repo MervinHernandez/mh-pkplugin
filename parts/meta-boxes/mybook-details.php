9854dd20ffd4b194b06f384460e17f1315d38db1
Mervin Hernandez <me@mervinhernandez.com>
2016-08-21 23:57:38 -0400

    mybook cleanup

--- parts/meta-boxes/mybook-details.php
+++ parts/meta-boxes/mybook-details.php
@@ -5,36 +5,36 @@ Post Type: mybook
 Meta Box: true
 Lock: true
 */
-// Published Date : full date picker
+// Published Date | text | mybook_published
   piklist('field', array(
-     'type' => 'datepicker',
-     'field' => 'mybook_published',
-     'label' => 'Published Date',
-     'value' => date('M d, Y', time() + 604800), // set default value
-     'options' => array(
-       'dateFormat' => 'M d, yy'
-     )
-   ));
-// Rating : form one to five stars
-piklist('field', array(
-   'type' => 'select',
-   'field' => 'mybook_rating',
-   'label' => 'Rating',
-   'choices' => array(
-     '1' => '1 star'
-     ,'2' => '2 stars'
-     ,'3' => '3 stars'
-     ,'4' => '4 stars'
-     ,'5' => '5 stars'
-   )
- ));
-// Property : whether I own it or not
-piklist('field', array(
-   'type' => 'radio',
-   'field' => 'mybook_property',
-   'label' => 'Property',
-   'choices' => array(
-     'own' => 'Own It',
-     'want' => 'Want It',
-   )
- ));
+      'type' => 'text'
+      ,'field' => 'mybook_published'
+      ,'label' => 'Published Year'
+      ,'value' => '2005'
+      ,'attributes' => array(
+        'class' => 'text'
+      )
+  ));
+// Rating | select | mybook_rating
+  piklist('field', array(
+    'type' => 'select',
+    'field' => 'mybook_rating',
+    'label' => 'Rating',
+    'choices' => array(
+      '1' => '1 star'
+      ,'2' => '2 stars'
+      ,'3' => '3 stars'
+      ,'4' => '4 stars'
+      ,'5' => '5 stars'
+    )
+  ));
+// Property | select | mybook_property
+  piklist('field', array(
+    'type' => 'radio',
+    'field' => 'mybook_property',
+    'label' => 'Property',
+    'choices' => array(
+      'own' => 'Own It',
+      'want' => 'Want It',
+    )
+  ));
