937bc90d7694970570d99f306edbcbb437521830
Mervin Hernandez <me@mervinhernandez.com>
2016-08-21 22:47:13 -0400

    mybook subtitle into meta box file
    separated it

--- mybooks.php
+++ mybooks.php
@@ -35,16 +35,3 @@ My Books POST TYPE
     );
     return $post_types;
   }
-/* Book Author Field */
-  function my_add_sub_title() {
-  piklist('field', array(
-    'type' => 'text'
-    ,'field' => 'mybook_author'
-    ,'template' => 'field' // format the field without a label
-    ,'attributes' => array(
-      'class' => 'large-text'
-      ,'placeholder' => 'Book Author'
-    )
-  ));
-}
-add_action('edit_form_after_title', 'my_add_sub_title');
