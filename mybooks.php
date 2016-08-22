372ff4a4e5823d8bbe030283ee7090f529ec8ecf
Mervin Hernandez <me@mervinhernandez.com>
2016-08-21 20:14:57 -0400

    needless commit

--- mybooks.php
+++ mybooks.php
@@ -35,3 +35,22 @@ My Books POST TYPE
     );
     return $post_types;
   }
+<<<<<<< HEAD
+=======
+/* Book Author Field */
+  function my_add_sub_title() {
+  piklist('field', array(
+    'type' => 'text'
+    ,'field' => 'mybook_author'
+    , 'relate' => arra (
+    'scope' => 'mybook'
+  )
+    ,'template' => 'field' // format the field without a label
+    ,'attributes' => array(
+      'class' => 'large-text'
+      ,'placeholder' => 'Book Author'
+    )
+  ));
+}
+add_action('edit_form_after_title', 'my_add_sub_title');
+>>>>>>> parent of f8cecb6... Revert "mybook_author relate to mybook post type only"
